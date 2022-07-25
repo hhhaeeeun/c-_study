16400d3dc01f0faa02916eeabbae05d3bf09da39
HhhaEeeun <haeun9432@gmail.com>
2022-07-25 16:43:58 +0900

    second commit

--- 0725.cpp
+++ 0725.cpp
@@ -19,14 +19,14 @@ public:
 
 	//대입 연산자
 
-	const CMy& operator = (const CMy& _Other)
+	 CMy& operator = (const CMy& _Other)
 	{
 		m_i = _Other.m_i;
 		m_f = _Other.m_f;
 
 		return *this;
 	}
-	const CMy& operator + (const CMy& _Other)
+	CMy& operator + (const CMy& _Other)
 	{
 		m_i += _Other.m_i;
 		m_f += _Other.m_f;
@@ -69,6 +69,39 @@ int main()
 	//해당 클래스의 객체(instance)가 필요함
 	//int(자료형) a(객체)
 	//멤버함수를 객체를 통해서 호출하면, 해당 객체의 주소가 this 포인터로 전달된다.
+
+	{
+		//자료형* 변수명;            포인터 변수 선언
+		//*포인터변수;                 포인터 주소 역참조
+		//&변수;                              변수의 주소값
+		//자료형& 변수명;;          레퍼런스 변수 선언
+
+
+		//C++에 추가된 기능
+		//포인터와 유사
+		//원본을 참조한다.
+		//참조변수를 통해서 직접적으로 원본을 수정 할 수 있다. 
+
+		int a = 10;
+
+		int* p = &a;
+		*p = 100;
+
+		int& iRef = a;
+		iRef = 100;
+		//레퍼런스 변수 장점
+		//한번 참조 이후 변경이 불가능 int* const p = &a 와 같음
+		//역참조 없이 값을 수정할 수 있음
+		//호출방식이 다름
+		//주소를 받는 것이 아니라 그 자체가 됨
+
+		const int* p2 = &a;
+		//*p2 = 100;
+
+		const int& iRefConst = a;
+		//const int* const p2 = &a; 와 같음
+		//iRefConst = 100;
+	}
 
 	tArr arr = {};
 	InitArr(&arr);
