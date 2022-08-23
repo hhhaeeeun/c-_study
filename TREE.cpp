c62fa82d6a5f8c8848e603d90be61ed512c583ea
HhhaEeeun <haeun9432@gmail.com>
2022-08-23 15:38:00 +0900

    TREE

--- TREE.cpp
+++ TREE.cpp
@@ -1,11 +1,45 @@
 #include <iostream>
 #include <map>
+#include<set>
+
+using std::cout;
+using std::endl;
 
 using std::map;
 using std::make_pair;
 
+using std::set;
+
+#define MAN 1
+#define WOMAN 2
+
+struct tStdInfo
+{
+	wchar_t szName[20];
+	unsigned char age;
+	unsigned char gender;
 
 
+	tStdInfo()
+		:szName{}
+		,age(0)
+		,gender(0)
+	{
+	}
+	tStdInfo(const wchar_t* _pName, unsigned char _age, unsigned char _gender)
+		:szName{}
+		, age(_age)
+		, gender(_gender)
+	{
+		wcscpy_s(szName, _pName);
+	}
+};
+
+struct pair
+{
+	const wchar_t* p;
+	tStdInfo info;
+};
 //inline
 //최적화 요청
 //코드를 복사해서 (호출하지 않고) 동작하도록 사용
@@ -30,10 +64,49 @@ int main()
 	//	-자가균형 기능 (AVL tree, Red/Black tree)
 
 
+	//Red/Black tree
+	//동적할당, 힙 메모리 영역 사용
+	set<int> setInt;
+	
+	setInt.insert(100);
+	setInt.insert(50);
+	setInt.insert(150);
+
+	//ROM 주소를 줌
+	const wchar_t* pStr = L"문자열";
+	
+
+	//2개의 타입 지정
+	//<키, 값>
+	map<int, float> mapData;
 
 
+	map<const wchar_t*, tStdInfo> stdMap;
+	tStdInfo info1(L"홍길동", 18, MAN);
+	tStdInfo info2(L"김하은", 25, WOMAN);
+
+	stdMap.insert(make_pair(L"홍길동", info1));
+	stdMap.insert(make_pair(L"김하은", info2));
 
 
+	map<const wchar_t*, tStdInfo>::iterator mapiter;
+	mapiter = stdMap.find(L"홍길동");
+
+	const wchar_t* tmp = (*mapiter).first;
+	tStdInfo temp = (*mapiter).second;
+
+	//mapiter->first
+	//mapiter->second 
+
+	//없는 값을 찾으면 end iterator로 반환됨
+	mapiter = stdMap.find(L"엄길동");
+
+	//데이터를 찾지 못함 
+	if (mapiter == stdMap.end())
+	{
+	
+		cout << "데이터를 찾을 수 없다." << endl;
+	}
 
 	return 0; 
 
